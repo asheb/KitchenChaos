024c5e423f429656dc42d978eeeee521d113b700
Alexander Sheboltaev <a.sheboltaev@gmail.com>
2023-03-05 10:09:05 +0700

    added character visual & rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,20 @@ public class Player : MonoBehaviour {
 
   private void Update() {
     Vector2 input = new();
-    if (Input.GetKey(KeyCode.W)) input.y += 1;
-    if (Input.GetKey(KeyCode.S)) input.y -= 1;
-    if (Input.GetKey(KeyCode.A)) input.x -= 1;
-    if (Input.GetKey(KeyCode.D)) input.x += 1;
+    //if (Input.GetKey(KeyCode.W)) input.y += 1;
+    //if (Input.GetKey(KeyCode.S)) input.y -= 1;
+    //if (Input.GetKey(KeyCode.A)) input.x -= 1;
+    //if (Input.GetKey(KeyCode.D)) input.x += 1;
+    if (Input.GetKey(KeyCode.E)) input.y += 1;
+    if (Input.GetKey(KeyCode.D)) input.y -= 1;
+    if (Input.GetKey(KeyCode.S)) input.x -= 1;
+    if (Input.GetKey(KeyCode.F)) input.x += 1;
     input.Normalize();
 
-    transform.position += new Vector3(input.x, 0, input.y) * moveSpeed * Time.deltaTime;
+    Vector3 moveDir = new(input.x, 0, input.y);
+    transform.position += Time.deltaTime * moveSpeed * moveDir;
+
+    if (input.magnitude > 0)
+      transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * 10);
   }  
 }
